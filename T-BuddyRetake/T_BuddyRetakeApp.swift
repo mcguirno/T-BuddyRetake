cf1ec9abc0c5a776c4ec6824a5a43077a99b744f
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-05-05 13:44:28 -0400

    Initial Commit

---
+++ T-BuddyRetake/T_BuddyRetakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_BuddyRetakeApp.swift
+//  T-BuddyRetake
+//
+//  Created by Noah McGuire on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct T_BuddyRetakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
